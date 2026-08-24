3db9dc263bd089d1d96ce86164f06342b321e24f
Jake <privatekodrp@gmail.com>
2026-08-24 08:52:08 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>    
+<head>
+        <meta charset="UTF-8">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="main.css">
+</head>
+<body class="homepage">
+    <h1 class="fancy-huge">Buenos dias</h1>
+    <p>login succesful du kek</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php test</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="login-container">
+    <h1>Sachen und Zeug</h1>
+    <form method="POST">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if ($username === 'deinemutter' && $password === 'penis') {
+            header("Location:home.php");
+            exit();
+        } else {
+            echo '<p class="error">wronggggg</p>';
+        }
+    }
+    ?>
+    </div>
+    </body>
+</html>
